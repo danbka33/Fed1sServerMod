deb7fbf1e3b9fbe78e8d98d65d032c01f734096b
Ajick <ajick2013@gmail.com>
2023-12-12 22:50:51 +0500

    fix: Changed permissions restrictions. Added "fix-permissions" command.

--- scripts/permissions.lua
+++ scripts/permissions.lua
@@ -10,115 +10,117 @@ Permissions.groups = {
 function Permissions.create_groups_and_apply_permissions()
     local default_group = game.permissions.get_group(Permissions.groups.default)
 
-    default_group.set_allows_action(defines.input_action.set_player_color, false)
-    default_group.set_allows_action(defines.input_action.start_research, false)
-    default_group.set_allows_action(defines.input_action.cancel_research, false)
-    default_group.set_allows_action(defines.input_action.set_research_finished_stops_game, false)
-    default_group.set_allows_action(defines.input_action.toggle_map_editor, false)
-    default_group.set_allows_action(defines.input_action.change_multiplayer_config, false)
-    default_group.set_allows_action(defines.input_action.add_permission_group, false)
-    default_group.set_allows_action(defines.input_action.edit_permission_group, false)
-    default_group.set_allows_action(defines.input_action.delete_permission_group, false)
-    default_group.set_allows_action(defines.input_action.import_permissions_string, false)
-    default_group.set_allows_action(defines.input_action.admin_action, false)
-    default_group.set_allows_action(defines.input_action.lua_shortcut, false)
-
-
-    if not game.permissions.get_group(Permissions.groups.manager) then
-        game.permissions.create_group(Permissions.groups.manager)
+    if default_group then
+        default_group.set_allows_action(defines.input_action.set_player_color, false)
+        default_group.set_allows_action(defines.input_action.start_research, false)
+        default_group.set_allows_action(defines.input_action.cancel_research, false)
     end
+
 
     local manager_group = game.permissions.get_group(Permissions.groups.manager)
 
-    manager_group.set_allows_action(defines.input_action.set_player_color, false)
-    manager_group.set_allows_action(defines.input_action.set_research_finished_stops_game, false)
-    manager_group.set_allows_action(defines.input_action.toggle_map_editor, false)
-    manager_group.set_allows_action(defines.input_action.change_multiplayer_config, false)
-    manager_group.set_allows_action(defines.input_action.add_permission_group, false)
-    manager_group.set_allows_action(defines.input_action.edit_permission_group, false)
-    manager_group.set_allows_action(defines.input_action.delete_permission_group, false)
-    manager_group.set_allows_action(defines.input_action.import_permissions_string, false)
-    manager_group.set_allows_action(defines.input_action.admin_action, false)
-    manager_group.set_allows_action(defines.input_action.lua_shortcut, false)
-
-
-    if not game.permissions.get_group(Permissions.groups.pick_role) then
-        game.permissions.create_group(Permissions.groups.pick_role)
+    if not manager_group then
+        manager_group = game.permissions.create_group(Permissions.groups.manager)
     end
+
+    if manager_group then
+        manager_group.set_allows_action(defines.input_action.set_player_color, false)
+    end
+
 
     local pick_role_group = game.permissions.get_group(Permissions.groups.pick_role)
 
-    for _, permission in pairs(defines.input_action) do
-        pick_role_group.set_allows_action(permission, false)
+    if not pick_role_group then
+        pick_role_group = game.permissions.create_group(Permissions.groups.pick_role)
     end
 
-    pick_role_group.set_allows_action(defines.input_action.gui_checked_state_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_click, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_confirmed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_elem_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_hover, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_leave, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_location_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_selected_tab_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_selection_state_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_switch_state_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_text_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.gui_value_changed, true)
-    pick_role_group.set_allows_action(defines.input_action.open_gui, true)
-    pick_role_group.set_allows_action(defines.input_action.start_walking, true)
-    pick_role_group.set_allows_action(defines.input_action.toggle_show_entity_info, true)
-    pick_role_group.set_allows_action(defines.input_action.translate_string, true)
-    pick_role_group.set_allows_action(defines.input_action.write_to_console, true)
+    if pick_role_group then
+        for _, permission in pairs(defines.input_action) do
+            pick_role_group.set_allows_action(permission, false)
+        end
 
-
-    --game.permissions.get_group(Permissions.groups.admin).add_player("fed1s")
-    --game.permissions.get_group(Permissions.groups.admin).add_player("danbka33")
-    --game.permissions.get_group(Permissions.groups.admin).add_player("Ajick")
-
-
-    if not game.permissions.get_group(Permissions.groups.admin) then
-        game.permissions.create_group(Permissions.groups.admin)
+        pick_role_group.set_allows_action(defines.input_action.gui_checked_state_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_click, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_confirmed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_elem_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_hover, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_leave, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_location_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_selected_tab_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_selection_state_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_switch_state_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_text_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.gui_value_changed, true)
+        pick_role_group.set_allows_action(defines.input_action.open_gui, true)
+        pick_role_group.set_allows_action(defines.input_action.start_walking, true)
+        pick_role_group.set_allows_action(defines.input_action.toggle_show_entity_info, true)
+        pick_role_group.set_allows_action(defines.input_action.translate_string, true)
+        pick_role_group.set_allows_action(defines.input_action.write_to_console, true)
     end
 
-    if game.is_multiplayer() then
-        return
-    end
 
     local admin_group = game.permissions.get_group(Permissions.groups.admin)
 
-    for _, player in pairs(game.players) do
-        if player.admin then
-            admin_group.add_player(player)
-        else
-            default_group.add_player(player)
+    if not admin_group then
+        admin_group = game.permissions.create_group(Permissions.groups.admin)
+    end
+
+    if admin_group then
+        for _, player in pairs(game.players) do
+            if player.admin then
+                admin_group.add_player(player)
+            elseif default_group then
+                default_group.add_player(player)
+            end
         end
     end
 end
 
-function Permissions.set_group(player, group)
-    if not player then
+function Permissions.set_group(player, group_name)
+    if not player or not group_name then
         return
     end
 
-    if not game.permissions.get_group(group) then
+    local group = game.permissions.get_group(group_name)
+
+    if not group then
         return
     end
 
-    game.permissions.get_group(group).add_player(player)
+    group.add_player(player)
 end
 
-function Permissions.in_group(player, group)
-    if not player then
+function Permissions.in_group(player, group_name)
+    if not player or not group_name then
         return false
     end
 
-    return player.permission_group.name == group
+    return player.permission_group.name == group_name
 end
 
 
 local event_handlers = {}
 event_handlers.on_init = Permissions.create_groups_and_apply_permissions
 EventHandler.add_lib(event_handlers)
+
+
+commands.add_command("fix-permissions", nil, function(command)
+    if not command or not command.player_index then
+        return
+    end
+
+    if game.is_multiplayer() then
+       return
+    end
+
+    local player = game.get_player(command.player_index)
+
+    if not player then
+        return
+    end
+
+    Permissions.set_group(player, Permissions.groups.admin)
+end)
 
 
 return Permissions
